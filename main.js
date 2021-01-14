ac71eeaf77fcb818eebd70431e6cb926d219419a
Mikihiro Kawahara <76198035+Mikihiro-Kawahara@users.noreply.github.com>
2021-01-14 18:35:06 +0900

    initial commit.
    ストップウォッチ

--- main.js
+++ main.js
@@ -1,19 +1,67 @@
-(function(){
-  'use strict';
-  var btn = document.getElementById('btn');
+let display = document.getElementById('display');
+let startStop = document.getElementById('startStop');
+let reset = document.getElementById('reset');
 
-  btn.addEventListener('click',function(){
-    var number = Math.random();
-    if(number < 0.3) {
-      this.textContent = '大吉';
-    } else if (number < 0.45) {
-      this.textContent = '吉';
-    } else if (number < 0.6) {
-      this.textContent = '中吉';
-    } else if (number < 0.85) {
-      this.textContent = '小吉';
-    } else {
-      this.textContent = '凶';
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+let newHours = 0;
+let newMinutes = 0;
+let newSeconds = 0;
+
+let status = "stop";
+let interval;
+
+function stopWatch(){
+  seconds++;
+  if(seconds / 60 == 1){
+    minutes++;
+    seconds = 0;
+    if(minutes / 60 == 1){
+      hours++;
+      minutes = 0;
     }
-  });
-}());
+  }
+  if(seconds < 10){
+    newSeconds = "0" + seconds;
+  }else{
+    newSeconds = seconds;
+  }
+  if(minutes < 10){
+    newMinutes = "0" + minutes;
+  }else{
+    newMinutes = minutes;
+  }
+  if(hours < 10){
+    newHours = "0" + hours;
+  }else{
+    newHours = hours;
+  }
+
+
+  display.innerHTML = newHours + ":" + newMinutes + ":" + newSeconds;
+}
+
+startStop.addEventListener("click", function(){
+  if(status == "stop"){
+    interval = setInterval(stopWatch, 100);
+    startStop.innerHTML ="STOP";
+    status = "start";
+  }
+  else{
+    clearInterval(interval);
+    startStop.innerHTML = "START";
+    status = "stop";
+  }
+})
+
+reset.addEventListener("click", function(){
+  clearInterval(interval);
+  startStop.innerHTML = "START";
+  status = "stop";
+  display.innerHTML ="00:00:00";
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+})
